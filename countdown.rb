83924e841b3dd0f4057dafee96faa69588cbaa4f
Woody Lucas <wlucas0311@gmail.com>
2018-11-25 04:27:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,8 +9,8 @@ end
 
 def countdown_with_sleep(num)
   while ( num > 0 )
-  secs -= 1 
-  puts "#{secs} SECOND(S)!"
+  num -= 1 
+  puts "#{num} SECOND(S)!"
   sleep(1)
 end
 return "HAPPY NEW YEAR!"
